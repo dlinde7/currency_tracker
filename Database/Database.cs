27322ab5601e96ba22fa814c3806e66263267bc9
Danie <danielinde7@gmail.com>
2021-06-17 13:46:16 +0200

    Database connection

---
+++ Database/Database.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace currency_tracker.Database
+{
+    public class Database
+    {
+        private MySqlConnection connection;
+        private string server;
+        private string database;
+        private string uid;
+        private string password;
+
+        public Database()
+        {
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            server = "130.211.215.193";
+            database = "currency";
+            uid = "root";
+            password = "trackcurrency";
+            string connectionString;
+            connectionString = "SERVER=" + server + ";" + "DATABASE=" +
+            database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+
+            connection = new MySqlConnection(connectionString);
+        }
+
+        private bool OpenConnection()
+        {
+        }
+
+        private bool CloseConnection()
+        {
+        }
+
+        public void Insert()
+        {
+        }
+
+        public void Update()
+        {
+        }
+
+        public void Delete()
+        {
+        }
+
+        public List<string>[] Select()
+        {
+        }
+    }
+}
